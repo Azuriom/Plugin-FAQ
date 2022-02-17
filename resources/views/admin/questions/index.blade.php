a60c424dcf35bb5956ed26622e4ddd334429a257
MrMicky <git@mrmicky.fr>
2022-02-17 18:10:55 +0100

    Fix tooltips

--- resources/views/admin/questions/index.blade.php
+++ resources/views/admin/questions/index.blade.php
@@ -57,8 +57,8 @@
                                         <span>{{ $question->name }}</span>
                                     </span>
                                     <span>
-                                        <a href="{{ route('faq.admin.questions.edit', $question) }}" class="m-1" title="{{ trans('messages.actions.edit') }}" data-toggle="tooltip"><i class="fas fa-edit"></i></a>
-                                        <a href="{{ route('faq.admin.questions.destroy', $question) }}" class="m-1" title="{{ trans('messages.actions.delete') }}" data-toggle="tooltip" data-confirm="delete"><i class="fas fa-trash"></i></a>
+                                        <a href="{{ route('faq.admin.questions.edit', $question) }}" class="m-1" title="{{ trans('messages.actions.edit') }}" data-bs-toggle="tooltip"><i class="fas fa-edit"></i></a>
+                                        <a href="{{ route('faq.admin.questions.destroy', $question) }}" class="m-1" title="{{ trans('messages.actions.delete') }}" data-bs-toggle="tooltip" data-confirm="delete"><i class="fas fa-trash"></i></a>
                                     </span>
                                 </div>
                             </div>
