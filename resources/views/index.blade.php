56a289ffe895f0ff8df0d52899f3b43a9c5b9186
MrMicky <git@mrmicky.fr>
2022-02-18 17:02:03 +0100

    Update default design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,31 +3,30 @@
 @section('title', trans('faq::messages.title'))
 
 @section('content')
-    <div class="container content">
-        <h1>{{ trans('faq::messages.title') }}</h1>
+    <h1>{{ trans('faq::messages.title') }}</h1>
 
-        @if($questions->isEmpty())
-            <div class="alert alert-info" role="alert">
-                {{ trans('faq::messages.empty') }}
-            </div>
-        @else
-            <div class="accordion" id="faq">
-                @foreach($questions as $question)
-                    <div class="card" id="{{ Str::slug($question->name) }}">
-                        <div class="card-header px-3 py-4" id="question-{{ $question->id }}">
-                            <a class="collapsed" data-bs-toggle="collapse" href="#answer-{{ $question->id }}" data-bs-target="#answer-{{ $question->id }}" aria-expanded="false" aria-controls="answer-{{ $question->id }}">
-                                {{ $question->name }}
-                            </a>
-                        </div>
+    @if($questions->isEmpty())
+        <div class="alert alert-info" role="alert">
+            {{ trans('faq::messages.empty') }}
+        </div>
+    @else
+        <div class="accordion" id="faq">
 
-                        <div id="answer-{{ $question->id }}" class="collapse" aria-labelledby="question-{{ $question->id }}" data-bs-parent="#faq">
-                            <div class="card-body user-html-content">
-                                {!! $question->answer !!}
-                            </div>
+            @foreach($questions as $id => $question)
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="{{ Str::slug($question->name) }}">
+                        <button class="accordion-button @if(!($show = ($id === 0))) collapsed @endif" type="button" data-bs-toggle="collapse" data-bs-target="#answer{{ $question->id }}" aria-expanded="false" aria-controls="answer{{ $question->id }}">
+                            {{ $question->name }}
+                        </button>
+                    </h2>
+                    <div id="answer{{ $question->id }}" class="accordion-collapse collapse @if($show) show @endif" aria-labelledby="{{ Str::slug($question->name) }}" data-bs-parent="#faq">
+                        <div class="accordion-body">
+                            {!! $question->answer !!}
                         </div>
                     </div>
-                @endforeach
-            </div>
-        @endif
-    </div>
+                </div>
+            @endforeach
+
+        </div>
+    @endif
 @endsection
