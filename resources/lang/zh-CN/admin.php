37b7b4122de03632f7f654fead32310d9e21e3aa
jhqwqmc <113768629+jhqwqmc@users.noreply.github.com>
2024-03-07 10:51:31 +0800

    Create admin.php

---
+++ resources/lang/zh-CN/admin.php
@@ -0,0 +1,13 @@
+return [
+    'title' => '常见问题',
+
+    'questions' => [
+        'title' => '问题',
+        'edit' => '编辑问题 #:question',
+        'create' => '创建问题',
+
+        'updated' => '问题顺序已更新。',
+    ],
+
+    'permission' => '管理常见问题',
+];
